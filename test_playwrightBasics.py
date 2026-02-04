c68bc487699465b37c2684235acd42b01db62e63
Hitesh Shimpi <hiteshshimpi@Hiteshs-MacBook-Air.local>
2026-02-04 20:16:48 +0530

    Assertiona and Firefox browser setup

--- test_playwrightBasics.py
+++ test_playwrightBasics.py
@@ -1,5 +1,6 @@
 from pytest_playwright.pytest_playwright import browser
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect,Playwright
+
 
 #! way to create context, browser and page ( use this in customer requirement)
 def test_plawyrightBasics(playwright):
@@ -16,3 +17,24 @@ def test_playwrightBasicsShortcut(page:Page):
         print("Navigating to Google...")
 
 
+#locators in playwright
+# css selectors --> if id then use #id and class then use .className
+def test_coreLocators(page:Page):
+        page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+        print("Navigating to Rahul shetty academy...")
+        print("Filling Username...")
+        page.get_by_label("username").fill("Hitesh")
+        print("Filling Password...")
+        page.get_by_label("password").fill("Learning")
+        page.get_by_role("combobox").select_option("consult") # combobox is for select class and option is the value from HTML
+        page.locator("#terms").check() #--> To just check the checkbox
+        page.get_by_role("button",name="Sign In").click()
+        expect(page.get_by_text("Incorrect username/password.")).to_be_visible()
+
+
+
+def test_firefoxbrowser(playwright : Playwright):
+        browser = playwright.firefox.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://www.google.com")
