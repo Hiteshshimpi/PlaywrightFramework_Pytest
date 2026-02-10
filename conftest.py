36858ed16c9416fa793600eee77be8c12596b45a
Hitesh Shimpi <hiteshshimpi2310@gmail.com>
2026-02-10 21:44:06 +0530

    New changes to file optimizing the code

--- conftest.py
+++ conftest.py
@@ -5,3 +5,26 @@ import pytest
 #request is used to return the parameter associated wth that testcase
 def usercred(request):
     return request.param
+
+@pytest.fixture(scope="function")
+def browserInstance(playwright,request):
+      # for golbal varaible
+    browser_name= request.config.getoption("--browser_name")
+    if browser_name == "chrome":
+        browser = playwright.chromium.launch(headless=False)
+    elif browser_name == "firefox":
+        browser = playwright.firefox.launch(headless=False)
+    elif browser_name == "safari":
+        browser = playwright.SafariWebDriver.launch(headless=False)
+
+    context = browser.new_context()
+    page = context.new_page()
+
+    yield page
+    #yeild it will execute till yield and it will come back after executing and then execute after yeild
+    context.close()
+    browser.close()
+
+# this will store the browser name, this is from the pytest
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action="store" ,default="chrome", help='Chrome or Firefox or Safari')
--- test_UiandApi.py
+++ test_UiandApi.py
@@ -2,7 +2,6 @@ import json
 
 import pytest
 from playwright.sync_api import Playwright, expect
-from pytest_playwright.pytest_playwright import browser
 
 from apiBase import ApiUtils
 from pageObjects.dashboard import DashboardPage
@@ -13,13 +12,9 @@ with open('data/credentails.json') as json_file:
     json_data = json.load(json_file)
     userCredentialsList = json_data['user_Credentials']
 
-
+@pytest.mark.smoke # using annotations
 @pytest.mark.parametrize('usercred',userCredentialsList)
-def test_e2e_web_api(playwright:Playwright,usercred):
-    browser=playwright.chromium.launch(headless=False)
-    context =browser.new_context()
-    page =context.new_page()
-
+def test_e2e_web_api(playwright:Playwright, browserInstance, usercred,):
     #create order - orderId
     api_Utils = ApiUtils() # creating object of that class
     responseody = api_Utils.createOrder(playwright)
@@ -30,7 +25,7 @@ def test_e2e_web_api(playwright:Playwright,usercred):
     userName =usercred["userEmail"]
     userPassword = usercred["userPassword"]
     #create object for login class
-    loginPage =LoginPage(page)
+    loginPage =LoginPage(browserInstance)
     loginPage.navigate_to_login_page()
     dashboardPage = loginPage.login(userName,userPassword)
 
@@ -48,4 +43,10 @@ def test_e2e_web_api(playwright:Playwright,usercred):
 
 
 
+# to execute
+#pytest -n 2 -m smoke --tracing on --html=report.html
 
+#-n is count for parallel execution
+#-m smoke it for annotation execution
+# --tracing on is for ss and logs
+# --html=report.html is for hmtl report
